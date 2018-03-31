dca9f6735f4fca6c07a0f6f0d506a0cd5fe383e0
Jonas Smith <30472987+JonasESmith@users.noreply.github.com>
2018-03-31 13:44:50 -0500

    Fewer threads

--- PatternGeneration.cs
+++ PatternGeneration.cs
@@ -24,7 +24,7 @@ namespace NurikabeApp
 
     private int totalNumberOfThreads
     {
-      get { return 5; }
+      get { return 2; }
     }
 
     // ************************************************************************************** //
